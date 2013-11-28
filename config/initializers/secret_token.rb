f8f1432f3d1e0a44444b99569d6ef10c9a236d15
Evan Coleman <evan@evancoleman.net>
2013-11-27 21:28:39 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+LightsRails::Application.config.secret_key_base = '93358c2e38857ca7f6037362c92fcefd7c48c61d9108f5f9e3e4aa4d4e3a34a8fef777d5d9cd91839994d084e50937342136c5cf5b8896c2aa0019e98cab9718'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+LightsRails::Application.config.session_store :cookie_store, key: '_lights-rails_session'
