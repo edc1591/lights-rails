08816790da26035b5095c7032b275b25b21d734b
Evan Coleman <evan@evancoleman.net>
2013-11-30 13:48:10 -0500

    fixed adding devices

--- app/controllers/x10devices_controller.rb
+++ app/controllers/x10devices_controller.rb
@@ -1,10 +1,10 @@
 class X10devicesController < ApplicationController
   def new
-  	
+  	@device = X10device.new
   end
 
   def create
-  	@device = X10device.new(params[:x10device])
+  	@device.update(params[:x10device])
   	@device.save
   	redirect_to index
   end
