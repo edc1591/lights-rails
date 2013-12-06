acb5718333ba0e12f56f46afbea620f7f6f07950
Evan Coleman <evan@evancoleman.net>
2013-12-06 10:46:25 -0500

    added some logging

--- config/events.rb
+++ config/events.rb
@@ -5,6 +5,7 @@ WebsocketRails::EventMap.describe do
   #
   # Uncomment and edit the next line to handle the client connected event:
      subscribe :client_connected, :to => WebsocketController, :with_method => :client_connected
+     subscribe :connection_closed, :to => WebsocketController, :with_method => :connection_closed
      subscribe :command, :to => WebsocketController, :with_method => :command
      subscribe :command_collection, :to => WebsocketController, :with_method => :command_collection
   #
