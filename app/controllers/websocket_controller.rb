f21ccd21199ef7583c0cdf1a91a6a78fdf38b3d7
Evan Coleman <evan@evancoleman.net>
2013-12-06 16:42:59 -0500

    h4x

--- app/controllers/websocket_controller.rb
+++ app/controllers/websocket_controller.rb
@@ -2,15 +2,22 @@ class WebsocketController < WebsocketRails::BaseController
 
 	def initialize_session
     # perform application setup here
-    # controller_store[:message_count] = 0
+    controller_store[:client_count] = 0
   end
 
   def client_connected
   	puts "client connected"
+    controller_store[:client_count] = controller_store[:client_count] + 1
   end
 
   def client_disconnected
     puts "client disconnected"
+    controller_store[:client_count] = controller_store[:client_count] - 1
+
+    if controller_store[:client_count] == 0
+      @ssh_test = Net::SSH.start(ENV["CLIENT_SSH_ADDRESS"],ENV["CLIENT_SSH_USER"], :password => ENV["CLIENT_SSH_PASSWORD"] ) do |ssh|
+      result = ssh.exec!('forever restart lights-mochad.js')
+    end
   end
 
   def command
