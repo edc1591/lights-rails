bddecef6c87de9b430c17ed2596e6f3877b73657
Evan Coleman <evan@evancoleman.net>
2013-12-23 17:57:05 -0500

    fix nil user

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -3,8 +3,10 @@ class Api::V1::UsersController < Api::V1::ApiController
 
 	def allowed_devices
 		retVal = Array.new
-		current_user.devices.each do |i|
-			retVal.push X10device.find_by_id(i)
+		unless current_user.nil?
+			current_user.devices.each do |i|
+				retVal.push X10device.find_by_id(i)
+			end
 		end
 		render :json => {:devices => retVal}
 	end
