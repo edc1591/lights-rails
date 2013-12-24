2f291fddbcaf10453a916da614f41b3350f3e4ed
Evan Coleman <evan@evancoleman.net>
2013-12-24 11:33:12 -0500

    save events in preset

--- app/controllers/api/v1/presets_controller.rb
+++ app/controllers/api/v1/presets_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::PresetsController < Api::V1::ApiController
 	before_filter :api_session_token_authenticate!
 
 	def create
-		@preset = Preset.new(:name => params[:name], :owner => current_user.id)
+		@preset = Preset.new(params[:preset], :owner => current_user.id)
 
 		if @preset.save
 			render :json => @preset, :code => :ok
@@ -16,7 +16,7 @@ class Api::V1::PresetsController < Api::V1::ApiController
 	end
 
 	def update
-		if Preset.update(params[:id], params[:events])
+		if Preset.update(params[:id], params[:preset])
     	render :json => Preset.find_by_id(params[:id]), :status => :ok
     else
     	render :json => {}, :code => :unprocessable_entity
