b431b93e744eb17996f6766d0d65efb2ea4a5185
Evan Coleman <evan@evancoleman.net>
2013-12-27 14:05:10 -0500

    why?

--- app/models/x10_device.rb
+++ app/models/x10_device.rb
@@ -1,4 +1,4 @@
 class X10Device < ActiveRecord::Base
-	attr_accessible :name, :deviceId, :houseCode, :deviceType, :zone
+	attr_accessible :name, :deviceId, :houseCode, :deviceType, :zone, :room_id
 	belongs_to :room
 end
