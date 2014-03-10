a227035a84bb7d0703e9305d3ba4201a4df531a1
Evan Coleman <evan@evancoleman.net>
2014-03-09 20:47:49 -0400

    fix typo

--- app/controllers/api/v1/rooms_controller.rb
+++ app/controllers/api/v1/rooms_controller.rb
@@ -21,10 +21,10 @@ class Api::V1::RoomsController < Api::V1::ApiController
 			object[:events].push d
 		end
 		if room.has_colors
-			if command == 0
+			if params[:command] == 0
 				d = {:color => [0,0,0], :eventType => 1}
 				object[:events].push d
-			elsif command == 1
+			elsif params[:command] == 1
 				d = {:brightness => 255, :speed => 200, :eventType => 2}
 				object[:events].push d
 			end
