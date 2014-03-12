e1c22f60f11e736a1bb6eb519de74f4d3c0f4300
Evan Coleman <evan@evancoleman.net>
2014-03-12 18:13:51 -0400

    dont nest beacons

--- app/controllers/api/v1/beacons_controller.rb
+++ app/controllers/api/v1/beacons_controller.rb
@@ -2,6 +2,6 @@ class Api::V1::BeaconsController < Api::V1::ApiController
 	before_filter :api_session_token_authenticate!
 
 	def show
-		render :json => {:beacons => Beacon.all}
+		render :json => Beacon.all
 	end
 end
