8ad587c7ef1265846ea3f5c3a36c17a928594041
Evan Coleman <evan@evancoleman.net>
2014-03-18 00:51:31 -0400

    send push notifications from admin panel

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -2,32 +2,24 @@ ActiveAdmin.register_page "Dashboard" do
 
   menu :priority => 1, :label => proc{ I18n.t("active_admin.dashboard") }
 
-  content :title => proc{ I18n.t("active_admin.dashboard") } do
-    div :class => "blank_slate_container", :id => "dashboard_default_message" do
-      span :class => "blank_slate" do
-        span I18n.t("active_admin.dashboard_welcome.welcome")
-        small I18n.t("active_admin.dashboard_welcome.call_to_action")
+  page_action :push, :method => :post do
+    params[:user_ids].each do |user_id|
+      notification = Houston::Notification.new(device: User.find_by_id(user_id).device_token)
+      notification.alert = params[:alert]
+      notification.badge = params[:badge]
+      notification.sound = params[:sound] unless params[:sound] == 'silent'
+      APN.push(notification)
+    end
+    redirect_to admin_dashboard_path, :notice => "Notification(s) Sent"
+  end
+
+  content do
+    columns do
+      column do
+        panel "Push Notifications" do
+          render partial: "send_push"
+        end        
       end
     end
-
-    # Here is an example of a simple dashboard with columns and panels.
-    #
-    # columns do
-    #   column do
-    #     panel "Recent Posts" do
-    #       ul do
-    #         Post.recent(5).map do |post|
-    #           li link_to(post.title, admin_post_path(post))
-    #         end
-    #       end
-    #     end
-    #   end
-
-    #   column do
-    #     panel "Info" do
-    #       para "Welcome to ActiveAdmin."
-    #     end
-    #   end
-    # end
-  end # content
+  end
 end
